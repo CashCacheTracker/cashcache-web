e0d6e1c5402d057c0d7ec5c8d2e63e4eba08063a
Jacob Courtneay <jacob@sporkexec.com>
2016-08-25 21:28:08 -0500

    Add new transaction route, transaction form

---
+++ app/components/transaction-form/component.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['transaction-form'],
+  transaction: null,
+
+  actions: {
+    cancel() { this.sendAction('cancel'); },
+    save() { this.sendAction('save'); },
+  },
+});
--- app/transaction/new/route.js
+++ app/transaction/new/route.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.get('store').createRecord('transaction');
+  },
+
+  actions: {
+    cancel: function() {
+      this.transitionTo('transaction');
+    },
+    save: function() {
+      console.log('save');
+    },
+  },
 });
