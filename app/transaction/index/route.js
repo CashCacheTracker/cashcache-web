4fed0261b95a6c8ddfd6e38207c8af00d34570b9
Jacob Courtneay <jacob@sporkexec.com>
2016-09-04 19:19:12 -0500

    Add transaction description, default date, sorting

--- app/transaction/index/route.js
+++ app/transaction/index/route.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.get('store').query('transaction', {
+      sort: '-date,-id', // Newest first, then newest id for collisions/invalid dates
     });
   }
 });
--- app/transaction/model.js
+++ app/transaction/model.js
@@ -1,9 +1,14 @@
 import DS from 'ember-data';
+import moment from 'moment';
+
+function getCurrentDate() {
+  return moment().format('YYYY-MM-DD');
+}
 
 export default DS.Model.extend({
   value: DS.attr('number'),
   note: DS.attr('string'),
-  date: DS.attr('string'),
+  date: DS.attr('string', { defaultValue: getCurrentDate }),
   description: DS.attr('string'),
   location: DS.attr('string'),
   is_split: DS.attr('boolean'),
