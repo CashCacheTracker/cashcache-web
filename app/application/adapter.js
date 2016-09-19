0eb21e524678f031745a304dc0f5aecaf4ad4a29
Jacob Courtneay <jacob@sporkexec.com>
2016-09-18 22:47:49 -0500

    Add JWT to API calls, initiate login by default

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -3,6 +3,14 @@ import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import config from '../config/environment';
 
 export default JSONAPIAdapter.extend({
+  session: Ember.inject.service(),
+
+  headers: Ember.computed('session.data.authenticated.jwt', function() {
+    let jwt = this.get('session.data.authenticated.jwt');
+    return {
+      'Authorization': `Bearer ${jwt}`,
+    };
+  }),
   host: config.apiUrl,
   pathForType: function(type) {
     return Ember.String.pluralize(Ember.String.underscore(type));
