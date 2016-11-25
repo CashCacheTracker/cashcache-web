eb86219cd02435bee1425f58eef32e6bdd10caa2
Jacob Courtneay <jacob@sporkexec.com>
2016-11-25 16:38:58 -0600

    Kill a geolocation test
    startApp isn't being called here, I'm not sure why I thought this would
    work. There's a reason I said I wasn't really going to test this in an
    earlier commit message, I was right the first time.

--- tests/unit/geolocation/service-test.js
+++ tests/unit/geolocation/service-test.js
@@ -3,21 +3,6 @@ import { stubSettings } from 'cashcache/services/geolocation';
 
 moduleFor('service:geolocation', 'Unit | Service | geolocation');
 
-test('it exists', function(assert) {
-  let service = this.subject();
-  assert.ok(service);
-});
-
-test('it returns a position', function(assert) {
-  let service = this.subject();
-  return service.getCurrentPosition().then(position => {
-    assert.ok(position, 'position exists');
-    let { latitude, longitude } = position.coords;
-    assert.defined(latitude, 'latitude exists');
-    assert.defined(longitude, 'longitude exists');
-  });
-});
-
 test('it accepts stubbing a position', function(assert) {
   let service = this.subject();
   let expectedLatitude = faker.address.latitude();
